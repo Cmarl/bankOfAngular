86448c010474a14bef50f114c668dac4aa385b74
Clyde G Marl <marl.clyde@gmail.com>
2015-04-27 10:16:28 -0700

    convert to boa shell

--- client/config/constants.js
+++ client/config/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 
-angular.module('poseidon')
-.constant('firebaseUrl', 'https://ENTER-YOUR-FB-URL.firebaseio.com/');
+angular.module('boa')
+.constant('firebaseUrl', 'https://bankoangular.firebaseio.com/');
